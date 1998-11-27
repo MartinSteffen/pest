5bb38633570be0bd27958fd042176a1ae6702a4f
swtech20 <msteffen@ifi.uio.no>
1998-11-27 10:04:07 +0000

    Neuer Konstruktor.

--- src/Absyn/Or_State.java
+++ src/Absyn/Or_State.java
@@ -3,16 +3,36 @@ package Absyn;
 public class Or_State extends State{
     public Statename     s_oname;
     public StateList     s_osubstates;
-    public TrList        s_odefaults;
+    public TrList        s_otrs;
+    public StatenameList s_odefaults;
     public ConnectorList s_oconnectors;
+    public Or_State(Statename osn,
+		    StateList osl,
+		    TrList otl,
+		    StatenameList osnl,
+		    ConnectorList ocl) {
+      s_oname = osn;
+      s_osubstates = osl;
+      s_otrs = otl;
+      s_odefaults = osnl;
+      s_oconnectors = ocl;
+    }
 }
 
 //----------------------------------------------------------------------
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Or_State.java,v 1.2 1998-11-26 16:32:20 swtech00 Exp $
+//	$Id: Or_State.java,v 1.3 1998-11-27 10:04:07 swtech20 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  1998/11/26 16:32:20  swtech00
+//	Id and Log extension
+//
 //
 //----------------------------------------------------------------------
+
+
+
+
+
