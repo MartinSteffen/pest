6a9a60b120f0949de7eeb8bedeaff59313fe15f5
swtech20 <msteffen@ifi.uio.no>
1998-11-27 10:07:18 +0000

    Klasse TrList public und Kontruktor public

--- src/Absyn/TrList.java
+++ src/Absyn/TrList.java
@@ -1,9 +1,9 @@
 package Absyn;
 
-class TrList { // List of transitions
+public class TrList { // List of transitions
     public Tr     head;
     public TrList tail;
-    TrList (Tr h, TrList tl) {
+    public TrList (Tr h, TrList tl) {
 	head = h;
 	tail = tail;
     };
@@ -13,8 +13,11 @@ class TrList { // List of transitions
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: TrList.java,v 1.2 1998-11-26 16:32:27 swtech00 Exp $
+//	$Id: TrList.java,v 1.3 1998-11-27 10:07:18 swtech20 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  1998/11/26 16:32:27  swtech00
+//	Id and Log extension
+//
 //
 //----------------------------------------------------------------------
