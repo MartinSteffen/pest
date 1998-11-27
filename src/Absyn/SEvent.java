c251128c5077cfa4e9638884c0c2882b7f8bb88d
swtech20 <msteffen@ifi.uio.no>
1998-11-27 09:59:04 +0000

    name public

--- src/Absyn/SEvent.java
+++ src/Absyn/SEvent.java
@@ -1,7 +1,7 @@
 package Absyn;
 
 public class SEvent {  
-    String name;
+    public String name;
     public SEvent (String n) {
 	name = n;
     }
@@ -13,8 +13,11 @@ public class SEvent {
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: SEvent.java,v 1.2 1998-11-26 16:32:22 swtech00 Exp $
+//	$Id: SEvent.java,v 1.3 1998-11-27 09:59:04 swtech20 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  1998/11/26 16:32:22  swtech00
+//	Id and Log extension
+//
 //
 //----------------------------------------------------------------------
