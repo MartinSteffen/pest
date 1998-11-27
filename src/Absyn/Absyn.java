fc9c3c2a76ee6f8eced7742d38faaf01ce07ac25
swtech20 <msteffen@ifi.uio.no>
1998-11-27 09:54:11 +0000

    Mergekonflikte aufgeloest.

--- src/Absyn/Absyn.java
+++ src/Absyn/Absyn.java
@@ -1,13 +1,9 @@
+package Absyn;
 
 abstract public class Absyn {
-    // <<<<<<< Absyn.java
  Coord coord;
  Location location;
 };
-=======
-  Coord coord;
-  Location location;
- };
 
 
 
@@ -26,12 +22,21 @@ abstract public class Absyn {
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Absyn.java,v 1.5 1998-11-27 08:53:03 swtech24 Exp $
+//	$Id: Absyn.java,v 1.6 1998-11-27 09:54:11 swtech20 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.5  1998/11/27 08:53:03  swtech24
+//	Zu State :  Erweiterung um : public Statename name
+//		    Zweck          : Schnellerer Zugriff auf Statenamen.
+//
+//	Zu Statename		   : Einfuegen des fehlenden public
+//
+//	Zu Absyn		   : keine Aenderungen, aber noch Probleme mit
+//				     Location und Coord.
+//				     Veraenderungen erst nach Besprechung.
+//
 //	Revision 1.4  1998/11/26 16:32:08  swtech00
 //	Id and Log extension
 //
 //
 //----------------------------------------------------------------------
-// >>>>>>> 1.4
