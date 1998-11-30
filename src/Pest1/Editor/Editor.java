3f59c03cee3cae5fd282750088d72080f7dca879
swtech24 <msteffen@ifi.uio.no>
1998-11-30 09:58:45 +0000

    Zentral-Konstruktor zum Aufruf des Editors. Hierbei kann root eine effektiver Statechartbaum, als auch ein null-Vektor sein. Die Namensvariable ist die Bezeichnung, die in der Titelleiste des Zeichenpads auftritt.

---
+++ src/Pest1/Editor/Editor.java
@@ -0,0 +1,23 @@
+/**
+ * Editor.java
+ *
+ *
+ * Created: Fri Nov 27 09:58:01 1998
+ *
+ * @author Software Technologie 24
+ * @version
+ */
+
+package Editor;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import Absyn.*;
+
+public class Editor {
+    
+    public Editor(Statechart root, String name) { }
+  
+
+} // Editor
