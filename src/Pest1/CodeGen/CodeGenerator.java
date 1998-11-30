5be5725fc796e6a5b7e9a90d264aacd949299957
swtech25 <msteffen@ifi.uio.no>
1998-11-30 12:25:14 +0000

    *** empty log message ***

---
+++ src/Pest1/CodeGen/CodeGenerator.java
@@ -0,0 +1,24 @@
+/*
+ * CodeGenerator
+ *
+ *
+ */
+package CodeGen;
+
+public class CodeGenerator
+{
+	public CodeGenerator()
+	{
+
+	}
+
+	public CodeGenerator(String path, Statechart statechart)
+	{
+		Generate(path, statechart);
+	}
+
+	public static boolean Generate(String path, Statechart statechart)
+	{
+
+	}
+}
