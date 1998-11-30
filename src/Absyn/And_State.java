cf3af495283f44a0d49b73acb5c7c749870c824a
swtech00 <msteffen@ifi.uio.no>
1998-11-30 17:07:40 +0000

    -  Namens-feld entfernt (da es nun in der Oberklasse ist) -  Konventionen vereinfacht ("_s" und "o"-Praefix entfernt)
    (Steffen)

--- src/Absyn/And_State.java
+++ src/Absyn/And_State.java
@@ -2,13 +2,12 @@ package Absyn;
 
 
 public class And_State extends State{
-    public Statename s_aname;
-    public StateList s_asubstates;
-    public And_State(Statename name,
-	      StateList substates) 
+    public StateList substates;
+    public And_State(Statename n,
+		     StateList sl) 
     {
-	s_aname = name;
-	s_asubstates = substates;
+	name = n;
+	substates = sl;
     }
 	
 }
@@ -16,8 +15,11 @@ public class And_State extends State{
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: And_State.java,v 1.2 1998-11-26 16:32:10 swtech00 Exp $
+//	$Id: And_State.java,v 1.3 1998-11-30 17:07:40 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  1998/11/26 16:32:10  swtech00
+//	Id and Log extension
+//
 //
 //----------------------------------------------------------------------
