9ef02a741f02380334523813c363a3286bc4edf2
swtech00 <msteffen@ifi.uio.no>
1998-11-30 17:08:55 +0000

    Namens-Feld entfernt, da es nun in der Oberklasse enthalten ist
    (Steffen)

--- src/Absyn/Basic_State.java
+++ src/Absyn/Basic_State.java
@@ -1,7 +1,6 @@
 package Absyn;
 
 public class Basic_State extends State{
-    public Statename name;
     public Basic_State (Statename n) {
 	name = n;
     }
@@ -11,8 +10,11 @@ public class Basic_State extends State{
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Basic_State.java,v 1.2 1998-11-26 16:32:11 swtech00 Exp $
+//	$Id: Basic_State.java,v 1.3 1998-11-30 17:08:55 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  1998/11/26 16:32:11  swtech00
+//	Id and Log extension
+//
 //
 //----------------------------------------------------------------------
