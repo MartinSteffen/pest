232b77296badb9382c22e2e03aefabe7ec76ee21
swtech26 <msteffen@ifi.uio.no>
1998-11-30 09:50:20 +0000

    Kommentar zu "Simu.java"
    Simu ist die einzige Schnittstelle, die wir zur Verfuegung
    stellen.
    
    Als Parameter erwartet Simu Daten vom Typ Statechart.

---
+++ src/Pest1/Simu/Simu.java
@@ -0,0 +1,19 @@
+/**
+ * Simu.java
+ *
+ *
+ * Created: Fri Nov 27 11:55:32 1998
+ *
+ * @author Software Technologie 26
+ * @version
+ */
+
+import Absyn.*;
+
+public class Simu {
+    
+    public Simu(Statechart Daten) {
+	        
+    }
+    
+} // Simu
