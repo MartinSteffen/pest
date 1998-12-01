71c73c57a2e7897b42b2ee8534a2d95ced7e8646
swtech22 <msteffen@ifi.uio.no>
1998-12-01 13:36:43 +0000

    *** empty log message ***

---
+++ src/Pest2/Simu/Simulation.java
@@ -0,0 +1,10 @@
+package Simu;
+
+/**Die allumfassende Simulation-Klasse*/
+import Absyn.*;
+public class Simulation extends Object{
+
+   /**Der Konstruktor: Wir erwarten zunaechst nur die aktuelle Statechart als Parameter. Spaeter koennen dann noch die Optionen und die Menus hinukommen. */
+   public Simulation(Statechart s){};
+
+}
