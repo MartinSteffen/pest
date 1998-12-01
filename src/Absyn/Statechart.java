e5fbc2856a2a43fec41f10415ea08f2509c8ee80
swtech00 <msteffen@ifi.uio.no>
1998-12-01 17:46:24 +0000

    Unverandert

--- src/Absyn/Statechart.java
+++ src/Absyn/Statechart.java
@@ -3,9 +3,9 @@ package Absyn;
 
 public class Statechart extends Absyn {
     public SEventList  events;
-    public BvarList bvars;
-    public PathList cnames;
-    public State    state;
+    public BvarList    bvars;
+    public PathList    cnames;
+    public State       state;
     public Statechart(SEventList el, 
 	       BvarList vl,
 	       PathList pl,
@@ -22,9 +22,12 @@ public class Statechart extends Absyn {
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Statechart.java,v 1.7 1998-11-26 17:11:18 swtech00 Exp $
+//	$Id: Statechart.java,v 1.8 1998-12-01 17:46:24 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.7  1998/11/26 17:11:18  swtech00
+//	*** empty log message ***
+//
 //
 //      Die Klasse Statechart wurde um einen neuen Konstruktor erweitert:
 //      public Statechart(String filename);
