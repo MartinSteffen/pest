0279bd6d9beb2744dc10ce6ecc52b379fd30f7fa
swtech22 <msteffen@ifi.uio.no>
1998-12-03 11:25:56 +0000

    Dummy-Konstruktor eingefuehrt, der Editor und Statechart akzeptiert und auf null prueft.

--- src/Pest2/Simu/Simulation.java
+++ src/Pest2/Simu/Simulation.java
@@ -12,5 +12,18 @@ public class Simulation extends Object{
 
   /** Konstruktor: Wir benoetigen die aktuelle Instanz des Editors, die Daten (Statechart). Was noch fehlt, sind die GUI-Elemente, wie
 z.B. Menus oder Optionen **/
-  public Simulation(Statechart s, Editor e){};
+  public Simulation(Statechart s, Editor e){
+    if (s!=null){
+      System.out.println("PEST2/Simulation hat eine Statechart erhalten");
+    }
+    else{
+      System.out.println("PEST2/Simulation : Statechart ist null????");
+    }
+    if (e!=null){
+      System.out.println("PEST2/Simulation hat ein Editorobject erhalten");
+    }
+    else{
+      System.out.println("PEST2/Simulation : Editor ist null????");
+    }
+  };
 }
