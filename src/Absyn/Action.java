389be77017e3500455bceb9ade91530f672f6888
swtech00 <msteffen@ifi.uio.no>
1998-12-03 17:06:51 +0000

    Klasse public gemacht

--- src/Absyn/Action.java
+++ src/Absyn/Action.java
@@ -4,18 +4,22 @@ package Absyn;
 /**
  * Abstract class Action introduces a union type for actions of transition labels.
  * @author Initially provided by Martin Steffen.
- * @version $Id: Action.java,v 1.4 1998-11-27 16:02:18 swtech00 Exp $
+ * @version $Id: Action.java,v 1.5 1998-12-03 17:06:51 swtech00 Exp $
  */
-abstract class  Action extends Absyn {
+public abstract class  Action extends Absyn {
 };
 
 //----------------------------------------------------------------------
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Action.java,v 1.4 1998-11-27 16:02:18 swtech00 Exp $
+//	$Id: Action.java,v 1.5 1998-12-03 17:06:51 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.4  1998/11/27 16:02:18  swtech00
+//	Ich habe damit angefangen, die Abstrakte Syntax so zu kommentieren, dass
+//	javadoc das auch verstehen kann. Das bereitet die Benutzung von javadoc vor.
+//
 //	Revision 1.3  1998/11/26 16:32:09  swtech00
 //	Id and Log extension
 //
