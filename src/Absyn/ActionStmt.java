47ac8288d294582f0b46cf30f4edaf9ab3c2aa7e
swtech00 <msteffen@ifi.uio.no>
1998-12-03 17:08:02 +0000

    Feld stmt public

--- src/Absyn/ActionStmt.java
+++ src/Absyn/ActionStmt.java
@@ -4,13 +4,13 @@ package Absyn;
 /**
  * One kind of transition action: boolean statement.
  * @author Initially provided by Martin Steffen.
- * @version $Id: ActionStmt.java,v 1.1 1998-12-01 17:52:53 swtech00 Exp $
+ * @version $Id: ActionStmt.java,v 1.2 1998-12-03 17:08:02 swtech00 Exp $
  */
 public class ActionStmt extends Action {
 /**
  * Contents of the statement.
  */
-  Boolstmt stmt;
+  public Boolstmt stmt;
   public ActionStmt  (Boolstmt st) {
     stmt = st;
   }
@@ -20,9 +20,18 @@ public class ActionStmt extends Action {
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: ActionStmt.java,v 1.1 1998-12-01 17:52:53 swtech00 Exp $
+//	$Id: ActionStmt.java,v 1.2 1998-12-03 17:08:02 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.1  1998/12/01 17:52:53  swtech00
+//	Anderungen in der Abstrakten Syntax:
+//
+//		- die _s-Klassen sind alle rausgeworfen, wie besprochen
+//		- gleichfalls _s-Felder
+//		- Die Operatorkonstanten in die jeweiligen Klassen als Konstanten
+//		  mit aufgenommen
+//		- Alle Klassen beginnen nun mit einem Gro"sbuchstaben
+//
 //	Revision 1.3  1998/11/27 16:14:31  swtech00
 //	Ich habe damit angefangen, die Abstrakte Syntax so zu kommentieren, dass
 //	javadoc das auch verstehen kann. Das bereitet die Benutzung von javadoc vor.
