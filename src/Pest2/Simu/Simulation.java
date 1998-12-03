2fb00e5dae5d55c58f62c13727fa687cd10df839
swtech22 <msteffen@ifi.uio.no>
1998-12-03 11:16:26 +0000

    *** empty log message ***

--- src/Pest2/Simu/Simulation.java
+++ src/Pest2/Simu/Simulation.java
@@ -1,10 +1,16 @@
 package Simu;
 
+
 /**Die allumfassende Simulation-Klasse*/
 import Absyn.*;
+import Editor.*;
+
 public class Simulation extends Object{
 
-   /**Der Konstruktor: Wir erwarten zunaechst nur die aktuelle Statechart als Parameter. Spaeter koennen dann noch die Optionen und die Menus hinukommen. */
+   /**Der alte Konstruktor: zunaechst nur die aktuelle Statechart als Parameter*/
    public Simulation(Statechart s){};
 
+  /** Konstruktor: Wir benoetigen die aktuelle Instanz des Editors, die Daten (Statechart). Was noch fehlt, sind die GUI-Elemente, wie
+z.B. Menus oder Optionen **/
+  public Simulation(Statechart s, Editor e){};
 }
