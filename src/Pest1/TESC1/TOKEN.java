26da25d734b37d366efccb7da6e70a39089328f8
swtech13 <msteffen@ifi.uio.no>
1998-12-07 14:06:41 +0000

    TOKEN-Klasse

---
+++ src/Pest1/TESC1/TOKEN.java
@@ -0,0 +1,25 @@
+package TESC1;
+
+class TOKEN {
+
+    protected String value_str;	                // Wert des Tokens
+    protected int    value_int; 
+    protected int token = vTOKEN.DUMMY;	        // Art des Tokens
+    protected int linenum;                      // Zeilennummer des Token
+
+    /*
+      //public-Methoden
+    public String getVal() {
+	return value;
+    }
+    
+    public int getToken() {
+	return token;
+    }
+    */
+}
+
+/* TOKEN
+ * $Id: TOKEN.java,v 1.1 1998-12-07 14:06:41 swtech13 Exp $
+ * $Log: not supported by cvs2svn $
+ */
