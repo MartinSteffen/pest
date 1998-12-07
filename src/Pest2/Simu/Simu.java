4c3772109871a966ea38696b0e3b3f053f71d595
swtech22 <msteffen@ifi.uio.no>
1998-12-07 14:58:07 +0000

    Wir haben die geforderten Änderungen durchgeführt: - es gibt nur noch die Klasse Simu - der Konstruktor bekommt keinen Editor mehr übergeben - unsere Kommentare sind schoener :-)

--- src/Pest2/Simu/Simu.java
+++ src/Pest2/Simu/Simu.java
@@ -1,29 +1,18 @@
 package Simu;
 
-
-/**Die allumfassende Simulation-Klasse. Wir haben aus Kompatibilitaetsgruenden 2 Klassen: Simu und Simulation.*/ 
 import Absyn.*;
-import Editor.*;
 
+/**swtech22 - Simulation: Die Simulationsumgebung wird durch einen einfachen Konstruktor gestartet*/ 
 public class Simu extends Object{
 
-   /**Der alte Konstruktor: zunaechst nur die aktuelle Statechart als Parameter*/
-   public Simu(Statechart s){};
-
-  /** Konstruktor: Wir benoetigen die aktuelle Instanz des Editors, die Daten (Statechart). Was noch fehlt, sind die GUI-Elemente, wie
-z.B. Menus oder Optionen **/
-  public Simu(Statechart s, Editor e){
+   /**Der Konstruktor: zunaechst nur die aktuelle Statechart als Parameter*/
+   /*Dummy: Es wird nur geprüft, ob s!=null ist.*/ 
+   public Simu(Statechart s){
     if (s!=null){
       System.out.println("PEST2/Simulation hat eine Statechart erhalten");
     }
     else{
       System.out.println("PEST2/Simulation : Statechart ist null????");
-    }
-    if (e!=null){
-      System.out.println("PEST2/Simulation hat ein Editorobject erhalten");
-    }
-    else{
-      System.out.println("PEST2/Simulation : Editor ist null????");
     }
   };
 }
