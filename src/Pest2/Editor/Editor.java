76791c422f918c31fb64b03f5026475dc0516565
swtech16 <msteffen@ifi.uio.no>
1998-12-07 15:24:06 +0000

    *** empty log message ***

---
+++ src/Pest2/Editor/Editor.java
@@ -0,0 +1,47 @@
+/*
+swtech16
+
+
+*/
+import java.awt.*;
+import java.awt.event.*;
+import Absyn.*;
+
+public class Editor extends Frame
+{
+    private Statechart statechart;
+    private String label;
+
+    public Editor(Statechart st, String l)
+    {
+	statechart = st;
+	label = l;
+	mainproc();
+    }
+
+    private void mainproc()
+    {
+	MenuBar mb = new MenuBar();
+	Menu File = new Menu("File");
+	mb.add(File);
+        setMenuBar(mb);
+	setSize(400,300);
+	show();
+        addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                Window window = e.getWindow();
+                window.dispose();
+                System.exit(0);
+            }
+        });
+
+    }
+
+    public static boolean highlight(Path obj)
+    {
+	return true;
+    }
+
+}
