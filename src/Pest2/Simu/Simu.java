cd8ac9ddd5dac02e20f8ca86624a8248e17e666f
swtech22 <msteffen@ifi.uio.no>
1998-12-07 12:38:11 +0000

    Neue Root-Klasse: Wir haben in Pest1 nachgesehen, um die Schnittstellen zu vereinfachen, brauchen wir evtl. Simu.java. Simu ist eine einfache Kopie von Simulation.

--- src/Pest2/Simu/Simu.java
+++ src/Pest2/Simu/Simu.java
@@ -1,18 +1,18 @@
 package Simu;
 
 
-/**Die allumfassende Simulation-Klasse. Wir haben aus Kompatibilitaetsgruenden 2 Klassen: Simulation und Simu*/  
+/**Die allumfassende Simulation-Klasse. Wir haben aus Kompatibilitaetsgruenden 2 Klassen: Simu und Simulation.*/ 
 import Absyn.*;
 import Editor.*;
 
-public class Simulation extends Object{
+public class Simu extends Object{
 
    /**Der alte Konstruktor: zunaechst nur die aktuelle Statechart als Parameter*/
-   public Simulation(Statechart s){};
+   public Simu(Statechart s){};
 
   /** Konstruktor: Wir benoetigen die aktuelle Instanz des Editors, die Daten (Statechart). Was noch fehlt, sind die GUI-Elemente, wie
 z.B. Menus oder Optionen **/
-  public Simulation(Statechart s, Editor e){
+  public Simu(Statechart s, Editor e){
     if (s!=null){
       System.out.println("PEST2/Simulation hat eine Statechart erhalten");
     }
