c69e1086cfa591edde6a29caf29ad6a0e1ccff11
swtech14 <msteffen@ifi.uio.no>
1998-12-07 13:39:27 +0000

    PrettyPrint-Beispiel

---
+++ src/Util/PrettyEx.java
@@ -0,0 +1,25 @@
+// Example for using PrettyPrint.
+// Written by Eike Schulz, 03-12-1998.
+
+package Util;
+
+
+import Absyn.Example;
+import Util.PrettyPrint;
+
+public class PrettyEx {
+
+  /**
+   * main-Methode -> Ausgabe Statechart-Example.
+   */
+  public static void main (String[] args) {
+
+    // Erzeuge PP-Objekt.
+    PrettyPrint pp = new PrettyPrint ();
+
+    // Starte PP-Ausgabe.
+    pp.start (Example.getExample());
+
+  } // method main
+
+} // class PrettyEx
