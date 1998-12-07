9131f2e7f759df5b796c9d20648d8ce014ff7c75
swtech18 <msteffen@ifi.uio.no>
1998-12-07 15:08:07 +0000

    *** empty log message ***

---
+++ src/STM/HAImport/Patterns.java
@@ -0,0 +1,16 @@
+package PEST1.STM.HAImport;
+
+interface Patterns {
+
+  // static final String STRING = "(\"[a-zA-Z_\\-,\\s0-9\\*]+\")";
+  // static final String STRINGSET = "\\{([\"a-zA-Z_\\-,\\s0-9\\.]*)\\}";
+  // static final String MK_HA = "\\s*mk_ha\\((.*)\\)\\s*";
+  // static final String MK_SYSTEM = "mk_system\\(" + MK_HA + "," + MK_HA + ",.*\\)";
+
+  static final String STRING = "(\"[^\"]*\")";
+  static final String MAPSET = "\\[([^\\]]*)\\]";
+  static final String STRINGSET = "\\{([^\\}]*)\\}";
+  static final String MK_HA = "\\s*mk_ha\\((.*)\\)\\s*";
+  static final String MK_SYSTEM = "mk_system\\(" + MK_HA + "," + MK_HA + ",.*\\)";
+
+}
