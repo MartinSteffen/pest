b5cbb3b825a80dab9e38758c9c459845913c1d0a
swtech00 <msteffen@ifi.uio.no>
1998-12-11 17:38:39 +0000

    Clonable

--- src/Absyn/Absyn.java
+++ src/Absyn/Absyn.java
@@ -6,19 +6,27 @@ package Absyn;
  * <br> TESC input language may have locations.
  * <p>
  * @author Initially provided by Martin Steffen.
- * @version  $Id: Absyn.java,v 1.8 1998-12-02 12:29:02 swtech19 Exp $
+ * @version  $Id: Absyn.java,v 1.9 1998-12-11 17:38:39 swtech00 Exp $
  */
-abstract public class Absyn {
- Location location;
+abstract public class Absyn implements Cloneable {
+    Location location;
+
+    public abstract Object clone() throws  CloneNotSupportedException;
+
+
 };
 
 //----------------------------------------------------------------------
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Absyn.java,v 1.8 1998-12-02 12:29:02 swtech19 Exp $
+//	$Id: Absyn.java,v 1.9 1998-12-11 17:38:39 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.8  1998/12/02 12:29:02  swtech19
+//	Entfernen der allgemeinen Koordinaten, da nur vier Klassen der abstrakten
+//	Syntax Koordinaten benoetigen.
+//
 //	Revision 1.7  1998/11/27 16:01:32  swtech00
 //	Ich habe damit angefangen, die Abstrakte Syntax so zu kommentieren, dass
 //	javadoc das auch verstehen kann. Das bereitet die Benutzung von javadoc vor.
