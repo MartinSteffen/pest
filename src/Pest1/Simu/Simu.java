38709af86df12705fb09ee26a1434219eab3fb35
swtech26 <msteffen@ifi.uio.no>
1998-12-14 22:20:12 +0000

    Anpassung an die neue Schnittstelle Erste Funktionen

--- src/Pest1/Simu/Simu.java
+++ src/Pest1/Simu/Simu.java
@@ -1,22 +1,79 @@
-/**
- * Simu.java
- *
- *
- * Created: Fri Nov 27 11:55:32 1998
- *
- * @author Software Technologie 26
- * @version
- *
- */
-
 package Simu;
 
-import Absyn.*;
+/**
+ * This type was created in VisualAge.
+ * @author: SWTECH 26
+ * @version: $Id
+ */
 
-public class Simu {
-    public Statechart SDaten; 
-    public Simu(Statechart Daten) {
-	SDaten = Daten;	        
-    }
-    
-} // Simu
+import Absyn.*;
+import Editor.*;
+
+public class Simu extends Object{
+	
+/**
+ * Simu constructor comment.
+ */
+public Simu(Statechart Daten) {
+	Statechart SDaten = Daten;
+	if (SDaten != null) {
+		System.out.println("Statechart angekommen !");
+	}
+	else {
+		System.out.println("Keine Statechart angekommen ! Objekt war null");
+	}
+}
+
+/**
+ * This method was created in VisualAge.
+ */
+public void get_guard(Guard waechter) {
+}
+
+/**
+ * This method was created in VisualAge.
+ */
+public void getNext() {
+}
+
+/**
+ * This method was created in VisualAge.
+ * @param aktion Absyn.Action
+ */
+public void make_action(Action aktion) {
+}
+
+/**
+ * This method was created in VisualAge.
+ */
+public void makeTab(Statechart Daten) {
+}
+
+/**
+ * This method was created in VisualAge.
+ * @param Daten Absyn.Statechart
+ * @param eEdit Editor
+ */
+public void Simu(Statechart Daten, Editor eEdit) {
+/* Nur fuer Testlaeufe der GUI-Gruppe und Uebergabetests
+ * Test, ob das uebergebene Objekt null ist
+ */
+	if (Daten != null) {
+		System.out.println("Statechart angekommen !");
+	}
+	else {
+		System.out.println("Keine Statechart angekommen ! Objekt war null");
+	}
+	if (eEdit != null) {
+		System.out.println("Editorobjekt uebergeben !");
+	}
+	else {
+		System.out.println("Keine Editorreferenz bekommen !");
+	}
+}
+}
+
+
+
+
+
