e2d896ac0928a332ef36ad0ce2b86404022b4c9e
swtech22 <msteffen@ifi.uio.no>
1998-12-14 13:33:31 +0000

    Wir haben den neuen (alten) Konstruktor wieder hinzugefuegt, aus Kompatibilitaetsgruenden jedoch den vorherigen (vorerst) nicht geloescht. Jetzt akzeptieren wir ein Editorobject als zweiten Parameter.

--- src/Pest2/Simu/Simu.java
+++ src/Pest2/Simu/Simu.java
@@ -1,6 +1,7 @@
 package Simu;
 
 import Absyn.*;
+import Editor.*;
 
 /**swtech22 - Simulation: Die Simulationsumgebung wird durch einen einfachen Konstruktor gestartet*/ 
 public class Simu extends Object{
@@ -15,4 +16,25 @@ public class Simu extends Object{
       System.out.println("PEST2/Simulation : Statechart ist null????");
     }
   };
+
+
+   /**Der neue (alte) Konstruktor: die aktuelle Statechart und ein Editorobject als als Parameter*/
+   /*Dummy: Es wird nur geprüft, ob s!=null und e!=null ist.*/    
+  public Simu(Statechart s, Editor e){
+   if (s!=null){
+      System.out.println("PEST2/Simulation hat eine Statechart erhalten");
+    }
+    else{
+      System.out.println("PEST2/Simulation : Statechart ist null????");
+    }
+    if (e!=null){
+      System.out.println("PEST2/Simulation hat einen Editor erhalten");
+    }
+    else{
+      System.out.println("PEST2/Simulation : Editor ist null????");
+    }
+ };
+
+
+
 }
