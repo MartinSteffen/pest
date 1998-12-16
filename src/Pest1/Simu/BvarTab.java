27a5af35a9cb6434855cb3112061714abc5b28aa
swtech26 <msteffen@ifi.uio.no>
1998-12-16 13:42:46 +0000

    CVS:
    Klasse zum Erstellen eine Bvar-Liste

---
+++ src/Pest1/Simu/BvarTab.java
@@ -0,0 +1,20 @@
+package simu;
+
+/**
+ * Klasse Tabelle-BVar
+ */
+
+import java.util.*;
+import java.lang.*;
+ 
+public class BvarTab {
+/**
+ * BvarTab constructor comment.
+ */
+public BvarTab(String strList, boolean boolWert) {
+	String sList;
+	boolean bWert;
+	sList = strList;
+	bWert = boolWert;
+}
+}
