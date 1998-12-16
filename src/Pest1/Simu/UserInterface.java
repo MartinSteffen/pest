5ce0f02ede0cd4dd3cafb3b90a3b03f3a05dd424
swtech26 <msteffen@ifi.uio.no>
1998-12-16 13:39:01 +0000

    Checkboxen fuer synchronen/asynchronen Ablauf hinzugefuegt

--- src/Pest1/Simu/UserInterface.java
+++ src/Pest1/Simu/UserInterface.java
@@ -1,6 +1,8 @@
 package simu;
 
 /**
+ * UserInterface
+ * Implementiert das Steuerungsfenster
  */
 
 import java.awt.*;
@@ -18,6 +20,9 @@ public class UserInterface {
 	private Panel ivjPanel1 = null;
 	private Panel ivjPanel2 = null;
 	private TextField ivjTextField1 = null;
+	private Checkbox ivjCheckbox1 = null;
+	private Checkbox ivjCheckbox2 = null;
+	private Panel ivjPanel3 = null;
 /**
  * Constructor
  */
@@ -30,12 +35,13 @@ public UserInterface() {
  * Return the Button1 property value.
  * @return java.awt.Button
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private Button getButton1() {
 	if (ivjButton1 == null) {
 		try {
 			ivjButton1 = new java.awt.Button();
 			ivjButton1.setName("Button1");
-			ivjButton1.setBounds(209, 8, 90, 23);
+			ivjButton1.setBounds(222, 7, 90, 23);
 			ivjButton1.setActionCommand("Button 1");
 			ivjButton1.setLabel("Schliessen");
 			// user code begin {1}
@@ -52,6 +58,7 @@ private Button getButton1() {
  * Return the Button2 property value.
  * @return java.awt.Button
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private Button getButton2() {
 	if (ivjButton2 == null) {
 		try {
@@ -73,6 +80,7 @@ private Button getButton2() {
  * Return the Button3 property value.
  * @return java.awt.Button
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private Button getButton3() {
 	if (ivjButton3 == null) {
 		try {
@@ -94,6 +102,7 @@ private Button getButton3() {
  * Return the Button4 property value.
  * @return java.awt.Button
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private Button getButton4() {
 	if (ivjButton4 == null) {
 		try {
@@ -112,9 +121,55 @@ private Button getButton4() {
 	return ivjButton4;
 }
 /**
+ * Return the Checkbox1 property value.
+ * @return java.awt.Checkbox
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private Checkbox getCheckbox1() {
+	if (ivjCheckbox1 == null) {
+		try {
+			ivjCheckbox1 = new java.awt.Checkbox();
+			ivjCheckbox1.setName("Checkbox1");
+			ivjCheckbox1.setBounds(57, 8, 127, 23);
+			ivjCheckbox1.setLabel("Synchroner Ablauf");
+			ivjCheckbox1.setState(true);
+			// user code begin {1}
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	};
+	return ivjCheckbox1;
+}
+/**
+ * Return the Checkbox2 property value.
+ * @return java.awt.Checkbox
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private Checkbox getCheckbox2() {
+	if (ivjCheckbox2 == null) {
+		try {
+			ivjCheckbox2 = new java.awt.Checkbox();
+			ivjCheckbox2.setName("Checkbox2");
+			ivjCheckbox2.setBounds(241, 9, 131, 23);
+			ivjCheckbox2.setLabel("Asynchroner Ablauf");
+			// user code begin {1}
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	};
+	return ivjCheckbox2;
+}
+/**
  * Return the ContentsPane property value.
  * @return java.awt.Panel
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private Panel getContentsPane() {
 	if (ivjContentsPane == null) {
 		try {
@@ -137,13 +192,14 @@ private Panel getContentsPane() {
  * Return the Frame1 property value.
  * @return java.awt.Frame
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private Frame getFrame1() {
 	if (ivjFrame1 == null) {
 		try {
 			ivjFrame1 = new java.awt.Frame();
 			ivjFrame1.setName("Frame1");
 			ivjFrame1.setLayout(new java.awt.BorderLayout());
-			ivjFrame1.setBounds(31, 27, 546, 240);
+			ivjFrame1.setBounds(43, 52, 546, 278);
 			ivjFrame1.setTitle("Simulator");
 			getFrame1().add(getContentsPane(), "Center");
 			// user code begin {1}
@@ -160,13 +216,14 @@ private Frame getFrame1() {
  * Return the Panel1 property value.
  * @return java.awt.Panel
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private Panel getPanel1() {
 	if (ivjPanel1 == null) {
 		try {
 			ivjPanel1 = new java.awt.Panel();
 			ivjPanel1.setName("Panel1");
 			ivjPanel1.setLayout(null);
-			ivjPanel1.setBounds(6, 195, 535, 37);
+			ivjPanel1.setBounds(6, 235, 535, 37);
 			getPanel1().add(getButton1(), getButton1().getName());
 			// user code begin {1}
 			// user code end
@@ -182,17 +239,19 @@ private Panel getPanel1() {
  * Return the Panel2 property value.
  * @return java.awt.Panel
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private Panel getPanel2() {
 	if (ivjPanel2 == null) {
 		try {
 			ivjPanel2 = new java.awt.Panel();
 			ivjPanel2.setName("Panel2");
 			ivjPanel2.setLayout(null);
-			ivjPanel2.setBounds(6, 5, 532, 185);
+			ivjPanel2.setBounds(6, 5, 532, 225);
 			getPanel2().add(getButton2(), getButton2().getName());
 			getPanel2().add(getButton3(), getButton3().getName());
 			getPanel2().add(getButton4(), getButton4().getName());
 			getPanel2().add(getTextField1(), getTextField1().getName());
+			getPanel2().add(getPanel3(), getPanel3().getName());
 			// user code begin {1}
 			// user code end
 		} catch (java.lang.Throwable ivjExc) {
@@ -204,15 +263,40 @@ private Panel getPanel2() {
 	return ivjPanel2;
 }
 /**
+ * Return the Panel3 property value.
+ * @return java.awt.Panel
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private Panel getPanel3() {
+	if (ivjPanel3 == null) {
+		try {
+			ivjPanel3 = new java.awt.Panel();
+			ivjPanel3.setName("Panel3");
+			ivjPanel3.setLayout(null);
+			ivjPanel3.setBounds(25, 144, 483, 40);
+			getPanel3().add(getCheckbox1(), getCheckbox1().getName());
+			getPanel3().add(getCheckbox2(), getCheckbox2().getName());
+			// user code begin {1}
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	};
+	return ivjPanel3;
+}
+/**
  * Return the TextField1 property value.
  * @return java.awt.TextField
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private TextField getTextField1() {
 	if (ivjTextField1 == null) {
 		try {
 			ivjTextField1 = new java.awt.TextField();
 			ivjTextField1.setName("TextField1");
-			ivjTextField1.setBounds(210, 93, 51, 42);
+			ivjTextField1.setBounds(210, 94, 51, 42);
 			// user code begin {1}
 			// user code end
 		} catch (java.lang.Throwable ivjExc) {
@@ -236,6 +320,7 @@ private void handleException(Throwable exception) {
 /**
  * Initialize the class.
  */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
 private void initialize() {
 	// user code begin {1}
 	// user code end
@@ -243,6 +328,7 @@ private void initialize() {
 	// user code end
 }
 /**
+ * This method was created in VisualAge.
  * 
  */
 public void start() {
