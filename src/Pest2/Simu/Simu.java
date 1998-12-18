4de09b8ff94f02bfcd37b91179781e08a98c7428
swtech22 <msteffen@ifi.uio.no>
1998-12-18 09:20:35 +0000

    Wir haben den endgueltigen Konstruktor eingefuehrt, alle anderen sind aber noch drin. Es sollte also keine Probleme mit dem Kompilieren geben.

--- src/Pest2/Simu/Simu.java
+++ src/Pest2/Simu/Simu.java
@@ -2,12 +2,27 @@ package simu;
 
 import absyn.*;
 import editor.*;
+import gui.*;
 
-/**swtech22 - Simulation: Die Simulationsumgebung wird durch einen einfachen Konstruktor gestartet*/ 
+/**swtech22 - Simulation: Die Simulationsumgebung wird durch einen einfachen Konstruktor gestartet
+ * <STRONG> Anforderungen. </STRONG> Wir verlassen uns darauf, dass die
+ * Statecharts, die uns uebergeben werden, folgende Eigenschaften haben:
+ *
+ * <ul>
+ * <li> Syntaktisch korrekte Statechart, wie im Pflichtenheft angegeben, d.h ohne Schleifen und andere Widrigkeiten (z.B. fehlende Start- oder Endpunkte von Transitionen)
+ * </ul>
+ *
+ * die mit folgenden Checks ueberprueft werden koennen:
+ *
+ * <ul>
+ * <li> checkModel(Statechart)
+ * </ul>
+ */
+         
 public class Simu extends Object{
 
-   /**Der Konstruktor: zunaechst nur die aktuelle Statechart als Parameter*/
-   /*Dummy: Es wird nur geprüft, ob s!=null ist.*/ 
+   /**Der alte Konstruktor: zunaechst nur die aktuelle Statechart als Parameter
+   *Dummy: Es wird nur geprüft, ob s!=null ist.*/ 
    public Simu(Statechart s){
     if (s!=null){
       System.out.println("PEST2/Simulation hat eine Statechart erhalten");
@@ -18,8 +33,8 @@ public class Simu extends Object{
   };
 
 
-   /**Der neue (alte) Konstruktor: die aktuelle Statechart und ein Editorobject als als Parameter*/
-   /*Dummy: Es wird nur geprüft, ob s!=null und e!=null ist.*/    
+   /**Der neue (alte) Konstruktor: die aktuelle Statechart und ein Editorobject als als Parameter
+    * Dummy: Es wird nur geprüft, ob s!=null und e!=null ist.*/    
   public Simu(Statechart s, Editor e){
    if (s!=null){
       System.out.println("PEST2/Simulation hat eine Statechart erhalten");
@@ -33,8 +48,27 @@ public class Simu extends Object{
     else{
       System.out.println("PEST2/Simulation : Editor ist null????");
     }
- };
+  }
+
+   /**Der gewuenschte Konstruktor: die aktuelle Statechart, ein Editorobject und das GUIInterface als Parameter
+    * Dummy: Es wird nur geprüft, ob s!=null und e!=null ist.*/    
+  public Simu(Statechart s, Editor e, GUIInterface gui){
+   if (s!=null){
+      System.out.println("PEST2/Simulation hat eine Statechart erhalten");
+    }
+    else{
+      System.out.println("PEST2/Simulation : Statechart ist null????");
+    }
+    if (e!=null){
+      System.out.println("PEST2/Simulation hat einen Editor erhalten");
+    }
+    else{
+      System.out.println("PEST2/Simulation : Editor ist null????");
+    }
+ }
 
 
 
 }
+
+
