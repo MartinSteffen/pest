70f8e7d43a389bf039f1cd7d42ded74036aa8f29
swtech22 <msteffen@ifi.uio.no>
1998-12-21 13:24:17 +0000

    Die endgueltige (?) Schnittstelle: nur noch ein Konstruktor.

--- src/Pest2/Simu/Simu.java
+++ src/Pest2/Simu/Simu.java
@@ -5,6 +5,7 @@ import editor.*;
 import gui.*;
 
 /**swtech22 - Simulation: Die Simulationsumgebung wird durch einen einfachen Konstruktor gestartet
+ * <P>
  * <STRONG> Anforderungen. </STRONG> Wir verlassen uns darauf, dass die
  * Statecharts, die uns uebergeben werden, folgende Eigenschaften haben:
  *
@@ -21,34 +22,6 @@ import gui.*;
          
 public class Simu extends Object{
 
-   /**Der alte Konstruktor: zunaechst nur die aktuelle Statechart als Parameter
-   *Dummy: Es wird nur geprüft, ob s!=null ist.*/ 
-   public Simu(Statechart s){
-    if (s!=null){
-      System.out.println("PEST2/Simulation hat eine Statechart erhalten");
-    }
-    else{
-      System.out.println("PEST2/Simulation : Statechart ist null????");
-    }
-  };
-
-
-   /**Der neue (alte) Konstruktor: die aktuelle Statechart und ein Editorobject als als Parameter
-    * Dummy: Es wird nur geprüft, ob s!=null und e!=null ist.*/    
-  public Simu(Statechart s, Editor e){
-   if (s!=null){
-      System.out.println("PEST2/Simulation hat eine Statechart erhalten");
-    }
-    else{
-      System.out.println("PEST2/Simulation : Statechart ist null????");
-    }
-    if (e!=null){
-      System.out.println("PEST2/Simulation hat einen Editor erhalten");
-    }
-    else{
-      System.out.println("PEST2/Simulation : Editor ist null????");
-    }
-  }
 
    /**Der gewuenschte Konstruktor: die aktuelle Statechart, ein Editorobject und das GUIInterface als Parameter
     * Dummy: Es wird nur geprüft, ob s!=null und e!=null ist.*/    
