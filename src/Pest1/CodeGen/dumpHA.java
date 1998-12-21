112f0721c4cb9f66e4b774f183787e07c270bccf
swtech25 <msteffen@ifi.uio.no>
1998-12-21 14:22:58 +0000

    Initial version.  Need to get stuff done.

---
+++ src/Pest1/CodeGen/dumpHA.java
@@ -0,0 +1,23 @@
+/**
+ * dumpHA
+ *
+ * This class is responsible for generating our hierarchical
+ * automaton.
+ *
+ * @version $Id: dumpHA.java,v 1.1 1998-12-21 14:22:57 swtech25 Exp $
+ * @author Marcel Kyas
+ */
+package codegen;
+
+public class dumpHA
+{
+	/**
+	 * In this section we will generate the new
+	 * events.
+	 *
+	 * @exception CodeGenException
+	 */
+	private void dumpNewEvents() throws CodeGenException
+	{
+	}
+}
---
+++ src/Pest1/CodeGen/dumpTables.java
@@ -0,0 +1,29 @@
+/**
+ * This class will dump the tables of the automaton.
+ */
+package codegen;
+
+/**
+ * This class will dump the tables of our code.  Internallz
+ * we will work with numerical values.  This is faster and
+ * more efficient.  But we want to provide the environment
+ * an interface to the original textual representations of
+ * states, events and conditions.
+ *
+ * @version $Id: dumpTables.java,v 1.1 1998-12-21 14:22:58 swtech25 Exp $
+ * @author Marcel Kyas
+ */
+public class dumpTables
+{
+	private void dumpStateTable ()
+	{
+	}
+
+	private void dumpEventTable()
+	{
+	}
+
+	private void dumpCondTable()
+	{
+	}
+}
