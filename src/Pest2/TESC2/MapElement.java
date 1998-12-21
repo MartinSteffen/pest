48c401eef798222917c0bf4127a01c1c32557495
swtech19 <msteffen@ifi.uio.no>
1998-12-21 13:18:02 +0000

    neue Unterklassen eingefuegt

--- src/Pest2/TESC2/MapElement.java
+++ src/Pest2/TESC2/MapElement.java
@@ -11,11 +11,11 @@
 
 package tesc2;
 
-import absyn.TrAnchor;
+import absyn.*;
 
 class MapElement  {
     
-    TrAnchor anchor = null;
+    protected TrAnchor anchor = null;
 
     // Default-Konstruktor, Dummy-Element
     MapElement() {
@@ -36,3 +36,60 @@ class MapElement  {
     }
 
 } // MapElement
+
+class MapState extends MapElement {
+
+    private State state = null;
+
+    // Konstruktor
+    MapState(State _state) {
+	state = _state;
+    }
+
+    TrAnchor getAnchor() {
+	return state.name;
+    }
+
+    public String toString() {
+	return this.getAnchor().toString();
+    }
+
+} // MapState
+
+class MapConnector extends MapElement {
+
+    private Connector con = null;
+
+    // Konstruktor
+    MapConnector(Connector _con) {
+	con = _con;
+    }
+
+    TrAnchor getAnchor() {
+	return con.name;
+    }
+
+    public String toString() {
+	return this.getAnchor().toString();
+    }
+
+} // MapConnector
+
+class MapUNDEF extends MapElement {
+
+    Point Position = null;
+
+    // Konstruktor
+    MapUNDEF(TrAnchor _anchor) {
+	anchor = _anchor;
+    }
+
+    TrAnchor getAnchor() {
+	return state.name;
+    }
+
+    public String toString() {
+	return this.getAnchor().toString();
+    }
+
+} // MapUNDEF
