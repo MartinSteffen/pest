055b1f886520117c844644471c57849049e3ea57
swtech00 <msteffen@ifi.uio.no>
1999-01-04 15:35:36 +0000

    SEvent als Unterklasse von Absyn (und damit mit Location)
    [Martin]

--- src/Absyn/SEvent.java
+++ src/Absyn/SEvent.java
@@ -2,7 +2,7 @@ package absyn;
 
 import java.io.Serializable;
 
-public class SEvent implements Serializable, Cloneable {  
+public class SEvent extends Absyn implements Serializable, Cloneable {  
     public String name;
     public SEvent (String n) {
 	name = n;
@@ -22,9 +22,12 @@ public class SEvent implements Serializable, Cloneable {
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: SEvent.java,v 1.7 1998-12-15 16:33:30 swtech00 Exp $
+//	$Id: SEvent.java,v 1.8 1999-01-04 15:35:36 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.7  1998/12/15 16:33:30  swtech00
+//	Towards new package names.
+//
 //	Revision 1.6  1998/12/15 13:38:07  swtech00
 //	exception-tag hinzugefuegt um javadoc sauber durchlaufen zu lassen
 //
