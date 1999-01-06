2b006ce903c9b621f542c59548edae8905edf300
swtech24 <msteffen@ifi.uio.no>
1999-01-06 15:02:53 +0000

    new Funktionen

--- src/Pest1/Editor/highlightObject.java
+++ src/Pest1/Editor/highlightObject.java
@@ -36,33 +36,41 @@ public class highlightObject {
 
     public highlightObject(Basic_State obj) { }
     public highlightObject(Basic_State obj, Color col) { temp = drawlist;
-						tempobj = new highLO(obj,Color.red);
+						tempobj = new highLO(obj,col);
 						drawlist = new highLList(tempobj,temp);
 						System.out.println("highlist : "+drawlist.head.type);}
 
     public highlightObject(And_State obj) { }
     public highlightObject(And_State obj, Color col) { 	temp = drawlist;
-						tempobj = new highLO(obj,Color.red);
+						tempobj = new highLO(obj,col);
 						drawlist = new highLList(tempobj,temp);
 						System.out.println("highlist : "+drawlist.head.type);}
    
     public highlightObject(Or_State obj) { }
     public highlightObject(Or_State obj, Color col) { 	temp = drawlist;
-						tempobj = new highLO(obj,Color.red);
+						tempobj = new highLO(obj,col);
 						drawlist = new highLList(tempobj,temp);
 						System.out.println("highlist : "+drawlist.head.type);}
 
     public highlightObject(Tr obj) { }
     public highlightObject(Tr obj, Color col) { 	temp = drawlist;
-					tempobj = new highLO(obj,Color.red);
+					tempobj = new highLO(obj,col);
 					drawlist = new highLList(tempobj,temp);
 					System.out.println("highlist : "+drawlist.head.type);}
 
     public highlightObject(Connector obj) { }
     public highlightObject(Connector obj, Color col) { 	temp = drawlist;
-						tempobj = new highLO(obj,Color.red);
+						tempobj = new highLO(obj,col);
 						drawlist = new highLList(tempobj,temp);
 						System.out.println("highlist : "+drawlist.head.type);}
+    public highlightObject(Absyn obj, Color col) {
+	if (obj instanceof Basic_State) {new highlightObject((Basic_State) obj,col);}
+	if (obj instanceof Or_State) {new highlightObject((Or_State) obj,col);}
+	if (obj instanceof And_State) {new highlightObject((And_State) obj,col);}
+	if (obj instanceof Tr) {new highlightObject((Tr) obj,col);}
+	if (obj instanceof Connector) {new highlightObject((Connector) obj,col);}
+
+	}
 
     public highlightObject(Graphics g,Statechart root) 
      {
