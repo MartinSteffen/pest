076108fe99cdc1d333f1cc624ef39866c32ad154
swtech14 <msteffen@ifi.uio.no>
1999-01-08 23:03:07 +0000

    Conname um CPoint erweitert.

--- src/Absyn/Conname.java
+++ src/Absyn/Conname.java
@@ -5,15 +5,29 @@ import java.io.Serializable;
 
 public class Conname extends TrAnchor implements Serializable, Cloneable{
     public String name;
+    public CPoint position;
+
     public Conname(String n) {
 	name  = n;
+	position = null;
+    };
+
+    public Conname(String n, CPoint p) {
+	name  = n;
+	position = p;
     };
     
 /**
  * @exception CloneNotSupportedException self-explanatory exception
  */
     public Object clone() throws CloneNotSupportedException {
-	return new Conname(name);
+      CPoint positionclone;
+      if (position != null) {
+	positionclone = (CPoint)position.clone();
+      } else
+	positionclone = null;
+
+	return new Conname(name, positionclone);
     };
 
 }
@@ -21,9 +35,12 @@ public class Conname extends TrAnchor implements Serializable, Cloneable{
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Conname.java,v 1.6 1998-12-15 16:33:26 swtech00 Exp $
+//	$Id: Conname.java,v 1.7 1999-01-08 23:03:07 swtech14 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.6  1998/12/15 16:33:26  swtech00
+//	Towards new package names.
+//
 //	Revision 1.5  1998/12/15 13:38:03  swtech00
 //	exception-tag hinzugefuegt um javadoc sauber durchlaufen zu lassen
 //
