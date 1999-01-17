6576322537e020540d9ef8cff435b37d267704b3
swtech11 <msteffen@ifi.uio.no>
1999-01-17 20:21:45 +0000

    *** empty log message ***

--- src/Pest1/Check/Crossreference.java
+++ src/Pest1/Check/Crossreference.java
@@ -34,23 +34,13 @@ import java.util.*;
  * keine
  * </DL COMPACT>
  * @author Java Praktikum: <a href="mailto:swtech11@informatik.uni-kiel.de">Gruppe 11</a><br>Daniel Wendorff und Magnus Stiller
- * @version  $Id: Crossreference.java,v 1.3 1999-01-12 17:25:40 swtech11 Exp $
+ * @version  $Id: Crossreference.java,v 1.4 1999-01-17 20:21:45 swtech11 Exp $
  */
 public class Crossreference extends ModelCheckBasics {
   private GUIInterface gui = null; // Referenz auf die GUI
   private Editor edit = null;
   private String such = new String("?");
-
-  private Vector rArt = new Vector(); // die Art des Elementes, evtl. überflüssig
-             //   1: OrState
-             //   2: AndState
-             //   3: BasicState
-
-  // das Objeckt selbst, z.B. zum Highlighten, evtl. überflüssig
-  private Vector rObj = new Vector();
-  // die texttuelle Art des Elementes plus seiner Beschreibung der Lage
-  private Vector rPth = new Vector();
-
+  private Vector items = new Vector();
 
   public Crossreference(GUIInterface _gui, Editor _edit) {
     gui = _gui;
@@ -69,10 +59,11 @@ public class Crossreference extends ModelCheckBasics {
     if (sc.state instanceof Basic_State) {navBasicState ((Basic_State)sc.state, null,""); }
 
     // Ausgabe
-    if (rPth.size()>0) {
+    if (items.size()>0) {
       gui.userMessage("Check: "+such+" ist ein:");
-      for (int i=0; i<rPth.size(); i++) {
-        gui.userMessage("Check: - "+(String)rPth.elementAt(i));
+      for (int i=0; i<items.size(); i++) {
+        ReportItem rp = (ReportItem)items.elementAt(i);
+        gui.userMessage("Check:   - "+rp.Pth);
       }
     }
     else {
@@ -82,34 +73,46 @@ public class Crossreference extends ModelCheckBasics {
 
 
   void navBasicState(Basic_State bs, State _s, String p) {
-    if (bs.name.name.equals(such)) {
-      rArt.addElement(new Integer(3));
-      rObj.addElement(bs);
-      rPth.addElement(new String("Basic-State in "+p));
-    }
+    if (bs.name.name.equals(such)) { itemInput(3,bs,"Basic-State in "+p); }
   }
 
   void navOrState(Or_State os, State _s, String p) {
-    if (os.name.name.equals(such)) {
-      rArt.addElement(new Integer(1));
-      rObj.addElement(os);
-      rPth.addElement(new String("Or-State in "+p));
-    }
+    if (os.name.name.equals(such)) { itemInput(1,os,"Or-State in "+p); }
     String np = getAddPathPart(p, os.name.name);
     if (os.trs != null) { navTransInTransList(os.trs, os, np); }
     if (os.substates != null) { navStateInStateList(os.substates, os, np); }
   }
 
   void navAndState(And_State as, State _s, String p) {
-    if (as.name.name.equals(such)) {
-      rArt.addElement(new Integer(2));
-      rObj.addElement(as);
-      rPth.addElement(new String("And-State in "+p));
-    }
+    if (as.name.name.equals(such)) { itemInput(2,as,"And-State in "+p); }
     String np = getAddPathPart(p, as.name.name);
     if (as.substates != null) { navStateInStateList(as.substates, as, np); }
   }
 
+  // Eingabe eines Report Ergebnises
+  void itemInput(int a, Object o, String p) {
+    ReportItem ri = new ReportItem(a, o, p);
+    items.addElement(ri);
+  }
 
+}
 
+class ReportItem {
+  // die Art des Elementes, evtl. überflüssig
+  int Art = 0;
+       //   0: unbekannt oder egal
+       //   1: OrState
+       //   2: AndState
+       //   3: BasicState
+
+  // das Objekt selbst, z.B. zum Highlighten, evtl. überflüssig
+  Object Obj = null;
+  // die texttuelle Art des Elementes plus seiner Beschreibung der Lage
+  String Pth = null;
+
+  public ReportItem(int a, Object o, String p) {
+    Art = a;
+    Obj = o;
+    Pth = p;
+  }
 }
