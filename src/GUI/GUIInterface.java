92591e7fc16769391486a9c4200297c3b9ec4333
swtech15 <msteffen@ifi.uio.no>
1999-01-18 14:41:47 +0000

    StateCharthasChanged ins Interface aufgenommen

--- src/GUI/GUIInterface.java
+++ src/GUI/GUIInterface.java
@@ -140,5 +140,12 @@ public interface GUIInterface{
      */  
 
     public Color getConnectorcolor();
+
+     /** StateChartHasChanged
+      * Diese Funktion dient dazu der GUI mitzuteilen, dass an den aktuellen Statechart Änderungen
+      * vorgenommen wurden.
+     */  
+
+    public void StateChartHasChanged();
 }
 
