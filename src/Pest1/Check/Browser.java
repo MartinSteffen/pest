0901a7a7ae3b3591db5495fbc051eeb81d220275
swtech11 <msteffen@ifi.uio.no>
1999-01-19 13:35:56 +0000

    *** empty log message ***

--- src/Pest1/Check/Browser.java
+++ src/Pest1/Check/Browser.java
@@ -8,7 +8,7 @@ import java.awt.event.*;
  * Browser
  *
  *  @author   Daniel Wendorff und Magnus Stiller
- *  @version  $Id: Browser.java,v 1.2 1999-01-19 10:46:06 swtech11 Exp $
+ *  @version  $Id: Browser.java,v 1.3 1999-01-19 13:35:56 swtech11 Exp $
  */
 public class Browser extends Dialog implements ActionListener {
 
@@ -33,38 +33,46 @@ public class Browser extends Dialog implements ActionListener {
     this.parent = parent;
 
     Point p = parent.getLocation();
-	  setLocation(p.x + 30 , p.y + 30);
+    setLocation(p.x + 30 , p.y + 30);
     setLayout(new BorderLayout());
-
     
     Panel output = new Panel(new BorderLayout());
 
-    output.add(new Label("Fehlermeldungen:"),"North");
-    List lst1 = new List();
-    lst1.setMultipleMode(true);
+
+    List lst = new List();
+    lst.setMultipleMode(true);
 
     if (mcm.getErrorNumber()>0) {
-
+    lst.add("Fehlermeldungen: ( Anzahl " + mcm.getErrorNumber() +  " ):");
       for (int i=1; (i<=mcm.getErrorNumber()); i++) {
-        String s = new String( mcm.getErrorMsg(i)+" ("+mcm.getErrorCode(i)+")" );
-        lst1.add(s);
+        lst.add(mcm.getErrorMsg(i)+" ("+mcm.getErrorCode(i)+")");
         // gui.userMessage("Check:   ["+mcm.etErrorPath(i)+"]");
       }
-
-      output.add(lst1,"Center");
     }
-/**    if (mcm.getWarningNumber()>0) {
-      gui.userMessage("Check:");
-      gui.userMessage("Check: Warnmeldungen ( Anzahl: " + getWarningNumber() +  " ):");
-      for (int i=1; (i<=getWarningNumber()); i++) {
-        gui.userMessage("Check: - "+getWarningMsg(i)+" ("+getWarningCode(i)+")" );
-        gui.userMessage("Check:   ["+getWarningPath(i)+"]");
+    if (mcm.getWarningNumber()>0) {
+    lst.add("Warnmeldungen: ( Anzahl " + mcm.getWarningNumber() +  " ):");
+
+      for (int i=1; (i<=mcm.getWarningNumber()); i++) {
+        lst.add(mcm.getWarningMsg(i)+" ("+mcm.getWarningCode(i)+")");
+	    //gui.userMessage("Check:   ["+getWarningPath(i)+"]");
       }
-    }*/
+    }
 
+    if (mcm.getWarningNumber()>0 | mcm.getErrorNumber()>0) { 
+  
+     Dimension d = lst.getMinimumSize();//lst.getRows());
+     System.out.println(d.height+" "+d.width);
+System.out.println(lst.getRows());
 
-     
-   add(output,"Center");
+      output.add(lst);
+     }    
+    
+
+     add(output,"Center");
+
+     Dimension  d = lst.getMinimumSize();//lst.getRows());
+     System.out.println(d.height+" "+d.width);
+System.out.println(lst.getRows());
 
 
     // Steuerungsbuttons
@@ -83,6 +91,7 @@ public class Browser extends Dialog implements ActionListener {
 
   	pack();
 	  setResizable(true);
+        setSize(d);
   	setVisible(true);
   }
 
