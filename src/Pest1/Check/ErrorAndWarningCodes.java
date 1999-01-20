cd654e58cbdf76d4999f4648e5130cac6b34bc2f
swtech11 <msteffen@ifi.uio.no>
1999-01-20 11:24:13 +0000

    *** empty log message ***

--- src/Pest1/Check/ErrorAndWarningCodes.java
+++ src/Pest1/Check/ErrorAndWarningCodes.java
@@ -2,7 +2,7 @@ package check;
 
 /**
  *  @author   Daniel Wendorff und Magnus Stiller
- *  @version  $Id: ErrorAndWarningCodes.java,v 1.27 1999-01-18 16:30:39 swtech11 Exp $
+ *  @version  $Id: ErrorAndWarningCodes.java,v 1.28 1999-01-20 11:24:11 swtech11 Exp $
  */
 class ErrorAndWarningCodes {
   ErrorAndWarningCodes() {
@@ -39,8 +39,8 @@ class ErrorAndWarningCodes {
       case  99: { _s = "Aufgrund eines fatalen Fehlers wird der Syntax Check abgebrochen."; break;  }
 
       case 100: {_s = "Doppelte Definition von BVar"; break; }
-      case 101: {_s = "Keine Deklaration von BVar"; break; }
-      case 102: {_s = "Deklarierte BVar wurde nicht verwendet"; break; }
+      case 101: {_s = "Keine Definition von BVar"; break; }
+      case 102: {_s = "Definierte BVar wurde nicht verwendet"; break; }
       case 103: {_s = "Der Boolsche Ausdruck im Guard der Transition laesst sich nicht spezifizieren."; break;}
       case 104: {_s = "Die Boolsche Variable wird in keinem Action veraendert."; break; }
       case 105: {_s = "Kein Guard fragt den Wert der boolschen Variable ab."; break; }
--- src/Pest1/Check/ModelCheck.java
+++ src/Pest1/Check/ModelCheck.java
@@ -49,7 +49,7 @@ import editor.*;
  * </DL COMPACT>
  *
  * @author Java Praktikum: <a href="mailto:swtech11@informatik.uni-kiel.de">Gruppe 11</a><br>Daniel Wendorff und Magnus Stiller
- * @version  $Id: ModelCheck.java,v 1.23 1999-01-19 22:57:35 swtech11 Exp $
+ * @version  $Id: ModelCheck.java,v 1.24 1999-01-20 11:24:13 swtech11 Exp $
  */
 public class ModelCheck {
   private ModelCheckMsg mcm; // Object, um die Fehler und Warnungen zu speichern
@@ -105,7 +105,7 @@ public class ModelCheck {
     boolean NoFatalError = false;
     boolean result = false;
 
-    boolean BrowserOut = false; //true; //   
+    boolean BrowserOut = true; //true; //   
 
     boolean Zeitnahme = true;
     long s0=0; long e0=0; long s1=0; long e1=0; long s2=0; long e2=0;
@@ -196,7 +196,7 @@ public class ModelCheck {
     mcm.sort(); // Meldungen sortieren
 
     if (BrowserOut==true) {
-      // CheckOption co = new CheckOption((pest)gui,new CheckConfig()); // zu Testzwecken
+	//CheckOption co = new CheckOption((pest)gui,new CheckConfig()); // zu Testzwecken
       Browser b = new Browser((pest)gui,mcm);
     }
     else if ( outputGUI == true ) { outputToGUI(); } // Ausgabe an die GUI
