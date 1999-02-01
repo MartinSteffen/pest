793373f9d86179b8705d49297c39998a59471c38
swtech00 <msteffen@ifi.uio.no>
1999-02-01 13:41:15 +0000

    public eingefuegt [Steffen]

--- src/Absyn/Ref_State.java
+++ src/Absyn/Ref_State.java
@@ -1,10 +1,8 @@
 package absyn;
-
 import java.io.Serializable;
-
 import java.awt.Rectangle;
 
-class Ref_State extends Basic_State {
+public class Ref_State extends Basic_State {
   /**
    * Name of the file
    */
@@ -34,7 +32,21 @@ class Ref_State extends Basic_State {
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Ref_State.java,v 1.1 1999-01-31 17:23:37 swtech00 Exp $
+//	$Id: Ref_State.java,v 1.2 1999-02-01 13:41:15 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.1  1999/01/31 17:23:37  swtech00
+//	Neuen ``Zustand'' (state) hinzugef"ugt:
+//
+//		Ref_State als Spezialfall eines Basic_States.
+//
+//
+//	Zu Angabe der ``importierten'' Syntax weitere Klassen hinzugef"ugt,
+//	die den Typ der Syntax beschreiben:
+//
+//	     Syntax_Type   ::=   Tesc_Syntax | Pest_AbSyntax
+//	     Pest_AbSyntax ::=   Pest_CoordSynax | Pest_NocoordSyntax
+//
+//	[Steffen]
+//
 //----------------------------------------------------------------------
