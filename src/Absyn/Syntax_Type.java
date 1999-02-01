96fd3acce9f9fdca005532be5caf051e1d10b413
swtech00 <msteffen@ifi.uio.no>
1999-02-01 14:33:46 +0000

    Kosmetik

--- src/Absyn/Syntax_Type.java
+++ src/Absyn/Syntax_Type.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 import java.awt.Rectangle;
 
-public abstract class Syntax_Type  extends Absyn implements Serializable, Cloneable {
+public abstract class Syntax_Type extends Absyn implements Serializable, Cloneable {
 /**
  * @exception CloneNotSupportedException self-explanatory exception
  */
@@ -16,9 +16,23 @@ public abstract class Syntax_Type  extends Absyn implements Serializable, Clonea
 //	Abstract Syntax for PEST Statecharts
 //	------------------------------------
 //
-//	$Id: Syntax_Type.java,v 1.1 1999-01-31 17:23:37 swtech00 Exp $
+//	$Id: Syntax_Type.java,v 1.2 1999-02-01 14:33:46 swtech00 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.1  1999/01/31 17:23:37  swtech00
+//	Neuen ``Zustand'' (state) hinzugef"ugt:
+//
+//		Ref_State als Spezialfall eines Basic_States.
+//
+//
+//	Zu Angabe der ``importierten'' Syntax weitere Klassen hinzugef"ugt,
+//	die den Typ der Syntax beschreiben:
+//
+//	     Syntax_Type   ::=   Tesc_Syntax | Pest_AbSyntax
+//	     Pest_AbSyntax ::=   Pest_CoordSynax | Pest_NocoordSyntax
+//
+//	[Steffen]
+//
 //----------------------------------------------------------------------
 
 
