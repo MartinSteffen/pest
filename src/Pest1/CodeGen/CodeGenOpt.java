fc5a8d9c45bb1005f9705f3a47d98f39bc795596
swtech25 <msteffen@ifi.uio.no>
1999-02-11 12:04:15 +0000

    added Serializable

--- src/Pest1/CodeGen/CodeGenOpt.java
+++ src/Pest1/CodeGen/CodeGenOpt.java
@@ -1,57 +1,60 @@
 /**
  * Container for Options to codegen.
  * @author Marcel Kyas, Walter Loeser, Andre Paetzold.
- * @version $Id: CodeGenOpt.java,v 1.2 1999-02-03 20:03:22 swtech25 Exp $
+ * @version $Id: CodeGenOpt.java,v 1.3 1999-02-11 12:04:15 swtech25 Exp $
  */
 package codegen;
 
-public class CodeGenOpt {
+import java.io.*;
+
+public class CodeGenOpt implements Serializable {
+
 	/**
 	 * Take the first Transition enabled.
 	 */
-	public final static int takeFirst = 0;
+	final static int takeFirst = 0;
 
 	/**
 	 * Try to cycle through enabled transitions.
 	 */
-	public final static int roundRobin = 1;
+	final static int roundRobin = 1;
 
 	/**
 	 * Choose an enabled transition randomly.
 	 */
-	public final static int random = 2;
+	final static int random = 2;
 
 	/**
 	 * Do not generate code for an environment.
 	 */
-	public final static int none = 0;
+	final static int none = 0;
 
 	/**
 	 * Generate the code for a simple environment.
 	 * This is an environment without user interaction.  It will
 	 * treat the statechart as a closed system.
 	 */
-	public final static int simple = 1;
+	final static int simple = 1;
 
 	/**
 	 * Current flavor of non-determinism handling
 	 */
-	public int nondetFlavor;
+	int nondetFlavor;
 
 	/**
 	 * Current flavor for environment.
 	 */
-	public int envFlavor;
+	int envFlavor;
 
 	/**
 	 * Do we want to generate a generation trace?
 	 */
-	public boolean traceCodeGen;
+	boolean traceCodeGen;
 
 	/**
 	 * Do we want to generate verbose output.
 	 */
-	public boolean verbose;
+	boolean verbose;
 
 	/**
 	 * Standard constructer.  We use the following defaults:
